422509e85f15869265f7ef2fb722ce35dfdedc40
jcd4stg <huynguyenlyn@gmail.com>
2023-12-09 19:06:57 +0700

    Initial setup

--- Dev43-IntroducingGit/ViewController.swift
+++ Dev43-IntroducingGit/ViewController.swift
@@ -11,7 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 20
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 20)
+        ])
+        
+        let foodCategoryLbl = UILabel()
+        foodCategoryLbl.text = "Food Category"
+        foodCategoryLbl.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        stackView.addArrangedSubview(foodCategoryLbl)
+        
+        let popularLbl = UILabel()
+        popularLbl.text = "Popular Dished"
+        popularLbl.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        stackView.addArrangedSubview(popularLbl)
+
     }
 
 
